9738ea2d8c8cf31539125e3271b1d4a24572f066
Rangga Aprilio <ranggaaprillio@gmail.com>
2021-05-08 08:19:21 +0700

    update search

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import {GetSurah} from '../helper/request' 
 import {Fragment, useEffect,useState} from 'react'
+import _ from "lodash";
 
 export default function Home(props) {
 
@@ -13,6 +14,20 @@ const [surah,setSurah]= useState([])
     console.log(data,"listing");
     setSurah(data)
   }, [])
+
+  const search=(data='')=>{
+    console.log(data,'data');
+    const res=props.list.allSurah.result.data
+    if (data=='') {
+      setSurah(res)
+      return false
+    }
+    let find = _.filter(res,function(item){
+      return item.name_latin.toLowerCase().indexOf(data.toLowerCase()) >= 0
+    })
+    console.log(find);
+    setSurah(find)
+  }
   
   return (
     <Fragment>
@@ -27,20 +42,20 @@ const [surah,setSurah]= useState([])
      
      <main className={styles.main}>
        <div className=" container fixed-top " style={{backgroundColor:"#51c4d3"}}>
-         <h1 className="fs-2 fw-2 my-2 font-custom " style={{color:"white"}}>Qur`an Online</h1>
-         <input className="form-control mb-3" type="text" placeholder="Cari nama surah" aria-label="default input example"></input>
+         <h1 className="fs-2 fw-2 my-2 font-custom " style={{color:"white"}}>Qur`an Online</h1> 
+         <input className="form-control mb-3" type="text" placeholder="Cari nama surah" aria-label="default input example" onChange={(e)=>search(e.target.value)}></input>
        </div>
        <div className="container" style={{ marginTop: 111 }}>
        <div className="row">
         {surah.map((v,i)=>(
           
-          <Fragment>
-          <div className="col-sm-6 mb-2">
+          <Fragment key={i}>
+          <div className="col-sm-6 mb-2" >
              <div className="card" >
-               <div className="card-body">
-                 <h4 dir="rtl" lang="ar"className="card-title" style={{fontFamily:'Amiri'}}>{v.name}</h4>
-                 <h6 className="card-subtitle mb-2 text-muted"><Link href={`/surah/${v.number}`}>{v.name_latin}</Link></h6>
-                 <p className="card-text">Jumlah Ayat:&nbsp;{v.number_of_ayah}</p> 
+               <div className="card-body" >
+                 <h4 dir="rtl" lang="ar"className="card-title" style={{fontFamily:'Amiri'}} >{v.name}</h4>
+                 <h6 className="card-subtitle mb-2 text-muted"><Link href={`/surah/${v.number}`} >{v.name_latin}</Link></h6>
+                 <p className="card-text" >Jumlah Ayat:&nbsp;{v.number_of_ayah}</p> 
                </div>
              </div>
            </div>
