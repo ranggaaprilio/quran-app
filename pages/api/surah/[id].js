e630003456eb3eb46e299964e251aa69a96c0db2
Rangga Aprilio <ranggaaprillio@gmail.com>
2021-05-18 21:46:58 +0700

    update ID

--- pages/api/surah/[id].js
+++ pages/api/surah/[id].js
@@ -1,11 +1,16 @@
 import fs from "fs";
+import path from "path"
 
 export default async(req, res) => {
    if (req.method === 'GET') {
        const {id}=req.query
        console.log(id,'ini id');
-    let rawdata = await fs.readFileSync(`data/surah/${id}.json`);
-    let surah = JSON.parse(rawdata);
+       const surahDirectory = path.join(process.cwd(), 'data/surah');
+       const filePath = path.join(surahDirectory, `${id}.json`);
+       let rawdata = await fs.readFileSync(filePath,'utf-8');
+       let surah = JSON.parse(rawdata);
+   //  let rawdata = await fs.readFileSync(`data/surah/${id}.json`);
+   //  let surah = JSON.parse(rawdata);
     res.status(200).json({result:surah})
    }
 }
