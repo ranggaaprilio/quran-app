bd1ad7967f4339e44f41f3891df628d383ea5393
Rangga Aprilio <ranggaaprillio@gmail.com>
2022-04-09 05:56:56 +0700

    change logic to FE

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,9 @@ import { Fragment, useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faKaaba, faQuran } from '@fortawesome/free-solid-svg-icons'
 import _ from "lodash";
+import { isUndefined } from 'lodash'
 import { useRouter } from 'next/router'
+import { DateTime } from 'luxon'
 
 export default function Home(props) {
 
@@ -16,11 +18,82 @@ export default function Home(props) {
   const [DefLocation, setDefLocation] = useState()
   const [active, setActive] = useState()
 
+
+  const parseToObject=(tanggal,waktu)=>{
+    let tanggalArray=tanggal.split('-');
+    let waktuArray=waktu.split(':');
+    return {year:tanggalArray[0],month:tanggalArray[1],day:tanggalArray[2],hour:waktuArray[0],minute:waktuArray[1]}
+}
+
+  const findNowJadwal=(dateAndTime)=>{
+    if(!isUndefined(dateAndTime.datetime[0])){
+      const now=DateTime.local().setZone('UTC+7');
+    const imsakObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Imsak);
+    const subuhObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Fajr);
+    const dzuhurObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Dhuhr);
+    const asharObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Asr);
+    const maghribObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Maghrib);
+    const isyaObject=parseToObject(dateAndTime?.datetime[0]?.date.gregorian,dateAndTime.datetime[0].times.Isha);
+
+    const imsak=DateTime.fromObject(imsakObject);
+    const subuh=DateTime.fromObject(subuhObject);
+    const dzuhur=DateTime.fromObject(dzuhurObject);
+    const ashar=DateTime.fromObject(asharObject);
+    const maghrib=DateTime.fromObject(maghribObject);
+    const isya=DateTime.fromObject(isyaObject);
+
+    console.log("compare",now<imsak);
+    console.log(now.toFormat('yyyy-MM-dd HH:mm:ss'),"DATE NOW");
+        if (now<imsak) {
+            return {
+                name:'Imsak',
+                time:imsak.toFormat('HH:mm')
+            }
+        } else if (now<subuh) {
+            return {
+                name:'Subuh',
+                time:subuh.toFormat('HH:mm')
+            }
+        }else if (now<dzuhur) {
+            return {
+                name:'Dzuhur',
+                time:dzuhur.toFormat('HH:mm')
+            }
+        }else if(now<ashar){
+            return {
+                name:'Ashar',
+                time:ashar.toFormat('HH:mm')
+            }
+        }else if(now<maghrib){
+            return {
+                name:'Maghrib',
+                time:maghrib.toFormat('HH:mm')
+            }
+        }else if(now<isya){
+            return {
+                name:'Maghrib',
+            }
+        }else{
+            return {
+                name:'Isya',
+                time:isya.toFormat('HH:mm')
+            }
+        }
+    }
+
+    return {
+      name:"Not Found",
+      time:""
+    }
+    
+}
+
   useEffect(() => {
     setDefaultJadwal(props.list.getTodaySchedule.datetime)
     setDefLocation(props.list.getTodaySchedule.location)
-    setActive(props.list.getTodaySchedule.active)
     console.log(props.list.getTodaySchedule)
+    const active=findNowJadwal(props.list.getTodaySchedule)
+    setActive(active)
   }, [])
 
   return (
